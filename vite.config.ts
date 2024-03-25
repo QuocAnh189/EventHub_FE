abecfef5bf537df8a24dc9e0c5d59a17dbfa2ba8
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2024-03-25 18:05:16 +0700

    set up overview (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,54 @@ import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin(), macrosPlugin()]
+  plugins: [react(), svgrPlugin(), macrosPlugin()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      treeshake: 'recommended',
+      output: {
+        manualChunks(id: any) {
+          if (id.includes('node_modules/antd')) {
+            return 'antd'
+          }
+          if (id.includes('node_modules/@mui')) {
+            return 'mui'
+          }
+          if (id.includes('node_modules/recharts')) {
+            return 'recharts'
+          }
+          if (id.includes('node_modules/react-select-country-list')) {
+            return 'country-list'
+          }
+          if (id.includes('node_modules/country-state-city')) {
+            return 'city-list'
+          }
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@layout': '/src/layout',
+      '@ui': '/src/ui',
+      '@pages': '/src/pages',
+      '@assets': '/src/assets',
+      '@styles': '/src/styles',
+      '@db': '/src/db',
+      '@hooks': '/src/hooks',
+      '@fonts': '/src/fonts',
+      '@utils': '/src/utils',
+      '@widgets': '/src/widgets',
+      '@contexts': '/src/contexts',
+      '@constants': '/src/constants'
+    }
+  },
+  define: {
+    // eslint-disable-next-line no-undef
+    'process.env': process.env
+  }
 })
