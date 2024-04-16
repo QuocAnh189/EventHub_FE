7af2f52fe8794fff00b38272fd026dd8ea350cc9
QuocAnh189 <anhquoc18092003@gmail.com>
2024-04-16 22:43:39 +0700

    update ui

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,12 +11,6 @@ const NotFound = () => {
         <h3 className='text-primary-500 font-bold text-2xl'>Oh No..</h3>
         <h2 className='text-[44px] font-bold mb-1'>An error occurred</h2>
         <p className='text-neutral-600 mb-6'>It appears this page does not exist or has been deleted.</p>
-        {/* <Link href={'/'}>
-          <Button className="text-white gap-2">
-            <LuHome />
-            Trở về trang chủ
-          </Button>
-        </Link> */}
       </div>
     </div>
   )
--- src/pages/event/Create-Event.tsx
+++ src/pages/event/Create-Event.tsx
@@ -1,12 +1,67 @@
+//hook
+import { useState } from 'react'
+
 // components
 import { PageHeader } from '@layouts/components'
-import ProductEditor from '@widgets/EventEditor'
+import Box from '@mui/material/Box'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+import InfomationEvent from '@components/common/event/InfomationEventCreate'
+import BannerEvent from '@components/common/event/BannerEventCreate'
+import TicketEventCreate from '@components/common/event/TicketEventCreate'
+import ReviewEventCreate from '@components/common/event/ReviewEventCreate'
 
 const CreateEvent = () => {
+  const steps = ['Infomation', 'Banner Image', 'Set Ticket', 'Review']
+  const [active, setActive] = useState<number>(0)
+
   return (
     <div className='min-h-screen'>
-      <PageHeader title='Event Creator' />
-      <ProductEditor />
+      <PageHeader title='Create New Event' />
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={active} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>
+                <p className='text-header'>{label}</p>
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+
+      {active === 0 && (
+        <InfomationEvent
+          setActive={(value) => {
+            setActive(value)
+          }}
+        />
+      )}
+
+      {active === 1 && (
+        <BannerEvent
+          setActive={(value) => {
+            setActive(value)
+          }}
+        />
+      )}
+
+      {active === 2 && (
+        <TicketEventCreate
+          setActive={(value) => {
+            setActive(value)
+          }}
+        />
+      )}
+
+      {active === 3 && (
+        <ReviewEventCreate
+          setActive={(value) => {
+            setActive(value)
+          }}
+        />
+      )}
     </div>
   )
 }
